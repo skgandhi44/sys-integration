78e03ff64229aa9941abd6b5706652229fde0bf9
Shivam <38300005+skgandhi44@users.noreply.github.com>
2019-10-11 19:13:15 -0400

    Clean up file

--- frontend/php/rabbitMQClient.php
+++ frontend/php/rabbitMQClient.php
@@ -1,14 +1,12 @@
 <?php
-    /*
-        This file creates new rabbit MQ client instances
-    */
-    //  Required files
+
     require_once('../rabbitmqphp_example/path.inc');
     require_once('../rabbitmqphp_example/get_host_info.inc');
     require_once('../rabbitmqphp_example/rabbitMQLib.inc');
+
     //  creates rabbitMq client instance for Database server
     function createClientForDb($request){
-        $client = new rabbitMQClient("../rabbitmqphp_example/rabbitMQ_db.ini", "testServer");
+        $client = new rabbitMQClient("../rabbitmqphp_example/testRabbitMQ.ini", "testServer");
         
         if(isset($argv[1])){
             $msg = $argv[1];
@@ -21,7 +19,5 @@
         
         return $response;
     }
-    //  creates rabbitMq client instance for Rabbot MQ server
-    
     
 ?>
