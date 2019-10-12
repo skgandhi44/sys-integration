6cb2360c6279f5255b52ff214fc415db76972ad1
Shivam <38300005+skgandhi44@users.noreply.github.com>
2019-10-11 21:02:55 -0400

    Remove extra white space

--- frontend/php/validation.php
+++ frontend/php/validation.php
@@ -8,56 +8,46 @@
 
     if (isset($_POST['reg_user'])) {
 		
-		$flname = $_POST['flname'];
+	$flname = $_POST['flname'];
         $email = $_POST['email'];
         $password = $_POST['password'];
 		
-        if (empty($flname)) 
-		{ 
-			array_push($errors, "Full Name is required"); 
-			
+        if (empty($flname)) { 
+		array_push($errors, "Full Name is required"); 
         } else {
-			if (!preg_match("/^[a-zA-Z ]*$/",$flname)) 
-			{
-			  array_push($errors, "Only letters and white space allowed");
-			}
+		if (!preg_match("/^[a-zA-Z ]*$/",$flname)) {
+			array_push($errors, "Only letters and white space allowed");
 		}
+	}
 	
-		if (empty("email")) 
-		{
-			array_push($errors, "Email is required"); 
+	if (empty("email")) {
+		array_push($errors, "Email is required"); 
 		
-		} else {
-			// check if e-mail address is well-formed
-			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
-			{
-			  array_push($errors, "Email address is invalid");
-			}
-		  }
+	} else {
+		// check if e-mail address is well-formed
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			array_push($errors, "Email address is invalid");
+		}
+	}
 		  
-		if (empty($password)) 
-		{ 
+	if (empty($password)) { 
             array_push($errors, "Password is required"); 
         }
 		
-        if ($password != $confirm_password) 
-		{
+        if ($password != $confirm_password) {
             array_push($errors, "The two passwords do not match");
         }
-	}
-
+    }
 
     if (isset($_POST['login_user'])) {
         $email = $_POST['email'];
         $password = $_POST['password'];
         
-        if(empty($email))
-        {
+        if(empty($email)){
            array_push($errors, 'Email is required');
         }
         
-        if(empty($password))
-        {
+        if(empty($password)){
             array_push($errors, 'Password is required');
         }
     }
