9cdf1774fb557db9613462c3370f401b3bea1b87
rdp48 <44008160+rdp48@users.noreply.github.com>
2019-10-15 20:45:04 -0400

    Auth works with rabbitMQ including form validation

--- frontend/php/validation.php
+++ frontend/php/validation.php
@@ -1,55 +1,100 @@
 <?php
 
+
+	require_once('../rabbitmqphp_example/path.inc');
+    	require_once('../rabbitmqphp_example/get_host_info.inc');
+    	require_once('../rabbitmqphp_example/rabbitMQLib.inc');
+    	require_once('rabbitMQClient.php');
+
+	session_start();
+	
 	$flname = ""; 
 	$email = "";
 	
 	$errors = array();
-
+	$request = array();
 
     if (isset($_POST['reg_user'])) {
+	$request["type"] = "register";
 		
 	$flname = $_POST['flname'];
         $email = $_POST['email'];
         $password = $_POST['password'];
-		
+
         if (empty($flname)) { 
 		array_push($errors, "Full Name is required"); 
-        } else {
-		if (!preg_match("/^[a-zA-Z ]*$/",$flname)) {
-			array_push($errors, "Only letters and white space allowed");
-		}
+        } 
+	elseif (!preg_match("/^[a-zA-Z ]*$/",$flname)){
+		array_push($errors, "Only letters and white space allowed");
+	} else {
+		$request["flname"] = $flname;
 	}
 	
-	if (empty("email")) {
+
+
+
+	if (empty($email)) {
 		array_push($errors, "Email is required"); 
-		
-	} else {
-		// check if e-mail address is well-formed
-		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			array_push($errors, "Email address is invalid");
-		}
+	} 
+	elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+		array_push($errors, "Email address is invalid");
+	} 
+	else {
+		$request["email"] = $email;
 	}
+
+
+
 		  
 	if (empty($password)) { 
             array_push($errors, "Password is required"); 
         }
-		
-        if ($password != $confirm_password) {
-            array_push($errors, "The two passwords do not match");
+
+	else {
+		$request["pass"] = $pass;
         }
+	
+	if (count($errors) == 0) {
+		$result = createClientForDb($request);
+		$_SESSION['email'] = $email;
+        	header('location: ../php/homepage.php');
+	}
+
     }
 
     if (isset($_POST['login_user'])) {
         $email = $_POST['email'];
         $password = $_POST['password'];
-        
-        if(empty($email)){
-           array_push($errors, 'Email is required');
+
+    	$request["type"] = "login";
+    	$request["email"] = $email;
+    	$request["pass"] = $pass;
+
+        if (empty($email)) {
+		array_push($errors, "Email is required"); 
+	} 
+	elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+		array_push($errors, "Email address is invalid");
+	} 
+	else {
+		$request["email"] = $email;
+	}
+
+		  
+	if (empty($password)) { 
+            array_push($errors, "Password is required"); 
         }
-        
-        if(empty($password)){
-            array_push($errors, 'Password is required');
+
+	else {
+		$request["pass"] = $pass;
         }
+	
+	if (count($errors) == 0) {
+		$result = createClientForDb($request);
+		$_SESSION['email'] = $email;
+        	header('location: ../php/homepage.php');
+	}
+
     }
 
 ?>
