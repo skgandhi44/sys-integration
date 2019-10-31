8d6e2fc20afdd80a152cdbd6debd18436f0dd11d
mrp72 <54955026+mrp72@users.noreply.github.com>
2019-10-31 03:48:37 -0400

    Minor changes to cases for DMZ function calls

--- dmz/php/dmzListner.php
+++ dmz/php/dmzListner.php
@@ -1,8 +1,9 @@
 <?php
     require_once('../rabbitmqphp_example/path.inc');
     require_once('../rabbitmqphp_example/get_host_info.inc');
-    require_once('../rabbitmqphp_example/rabbitMQLib.inc');
-    
+    require_once('../rabbitmqphp_example/rabbitMQLib.inc'); 
+    include('dmzFunctions.php');
+
     //Error logging
     error_reporting(E_ALL);
     
@@ -10,77 +11,31 @@
     ini_set('log_errors', 'On');
     ini_set('error_log', dirname(__FILE__). '/../logging/log.txt');
 
-function fetchItem($item){
-	
-	$curl = curl_init();
-	
-	curl_setopt_array($curl, array(
-		CURLOPT_URL => "https://trackapi.nutritionix.com/v2/search/instant?query=".$item,
-  		CURLOPT_RETURNTRANSFER => true,
-  		CURLOPT_ENCODING => "",
-  		CURLOPT_MAXREDIRS => 10,
-  		CURLOPT_TIMEOUT => 30,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  		CURLOPT_CUSTOMREQUEST => "GET",
-  		CURLOPT_HTTPHEADER => array(
-    		        "Postman-Token: 8901c377-f138-4052-a142-4ab83e7f62be",
-                    "cache-control: no-cache",
-                    "x-app-id: 62fe602e",
-                    "x-app-key: b374d3eba576afafbe1c0033f06a11cd"
-  		    ),
-        )
-    );
-	$response = curl_exec($curl);
-	$err = curl_error($curl);
-	curl_close($curl);
-	if($err) {
-		echo "CURL ERROR #:".$err;
-	}else{
-        echo "Search Bar Entry = ".$item;
-		$result = json_decode($response, true);
-        $commonFoods = $result['common'];
-        $food_details = $commonFoods['0'];
-        print_r($food_details);
-        return $food_details;
-	}
-}
+    function requestProcessor($request)
+    {
+        echo "received request".PHP_EOL;
+        echo "<br> type = " . $request['type'];
 
-function fetchNutrients($item){
-	
-	$curl = curl_init();
-	
-	curl_setopt_array($curl, array(
-		CURLOPT_URL => "https://trackapi.nutritionix.com/v2/natural/nutrients",
-  		CURLOPT_RETURNTRANSFER => true,
-  		CURLOPT_ENCODING => "",
-  		CURLOPT_MAXREDIRS => 10,
-  		CURLOPT_TIMEOUT => 30,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  		CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => "",
-  		CURLOPT_HTTPHEADER => array(
-    		        "Postman-Token: 8901c377-f138-4052-a142-4ab83e7f62be",
-                    "cache-control: no-cache",
-                    "x-app-id: 62fe602e",
-                    "x-app-key: b374d3eba576afafbe1c0033f06a11cd"
-  		    ),
-        )
-    );
-	$response = curl_exec($curl);
-	$err = curl_error($curl);
-	curl_close($curl);
-	if($err) {
-		echo "CURL ERROR #:".$err;
-	}else{
-        echo "Search Bar Entry = ".$item;
-		$result = json_decode($response, true);
-        $commonFoods = $result['common'];
-        $food_details = $commonFoods['0'];
-        print_r($food_details);
-        return $food_details;
-	}
-}
+        //var_dump($request);
+        
+      if(!isset($request['type']))
+      {
+        return "ERROR: unsupported message type";
+      }
+        
+      switch ($request['type'])
+      {
+          case "fetchItem":
+              echo "Search Food Case";
+              $response = fetchItem($request['search_item']);
+              break;
 
-
+      }
+        //echo $response;
+        return $response;
+    }
+    $server = new rabbitMQServer("../rabbitmqphp_example/testRabbitMQ.ini","testServer");
+    //echo "testRabbitMQServer BEGIN".PHP_EOL;
+    $server->process_requests('requestProcessor');
+    //exit();
 ?>
