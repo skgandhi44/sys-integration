f6ba0f16c3cb9c2da2a69be4bd53e9ef52ffd9fc
Shivam <38300005+skgandhi44@users.noreply.github.com>
2019-10-31 23:01:28 -0400

    Add addition html page to display user info

---
+++ frontend/html/userAccount.php
@@ -0,0 +1,53 @@
+<?php include('../php/validation.php');?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <title>User Information</title>
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+    </head>
+
+    <body id = "wrapper">
+       
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse btn-group" id="navbarSupportedContent">
+                <div class="btn-group">
+                    <a class="btn btn-primary" href="../php/homepage.php">Dashboard</a>
+                </div>
+            </div>
+        </nav>
+       
+        <div class = "jumbotron">
+            <header id = "page-header">
+                <h2 class = "text-white text-center font-weight-bold">User Information</h2>
+            </header>
+        </div>
+
+        <div class = "container">
+            <div class="card x-shadow mx-auto" style="max-width: 550px; margin-top: -6rem;">
+                <div class="card-body p-5">
+                    <a>
+                        Email: <strong><?php echo $_SESSION['email']; ?></strong><br>
+                        Full Name: <strong><?php echo $_SESSION['flname']; ?></strong>
+                    </a>
+                </div>
+            </div>
+        </div>
+       
+                <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
