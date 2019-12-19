f57851b3d37cc0aeb7f8c62c4a69266016fcf6d4
mrp72 <54955026+mrp72@users.noreply.github.com>
2019-12-18 19:48:50 -0500

    Distributed logs sent to FE and DMZ
    The logs for DB are sent to FE and DMZ using multiple RMQ queues.

--- database/logging/logging.php
+++ database/logging/logging.php
@@ -19,7 +19,8 @@
     $request = array();
     $request['type'] = "db";  
     $request['error_string'] = $errorArray;
-    $returnedValue = createClientForRmq($request);
+    $returnedValueFE = createClientForFE($request);
+    $returnedValueDMZ = createClientForDMZ($request);
     $fp = fopen("../logging/log_history.txt", "a");
 
     for($i = 0; $i < count($errorArray); $i++){
@@ -28,8 +29,23 @@
 
     file_put_contents("../logging/log.txt", "");
 
-    function createClientForRmq($request){
-        $client = new rabbitMQClient("../rabbitmqphp_example/rabbitMQ_rmq.ini", "testServer");
+
+    function createClientForFE($request){
+        $client = new rabbitMQClient("../rabbitmqphp_example/rabbitMQ_RMQ.ini", "DB2_RMQ_Log_Server");
+       
+        if(isset($argv[1])){
+            
+            $msg = $argv[1];
+        }
+        else{
+            $msg = "client";
+        }
+        $response = $client->send_request($request);
+        return $response;
+    }
+
+    function createClientForDMZ($request){
+        $client = new rabbitMQClient("../rabbitmqphp_example/rabbitMQ_RMQ.ini", "DB1_RMQ_Log_Server");
        
         if(isset($argv[1])){
             
