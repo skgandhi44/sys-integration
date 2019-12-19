5ac42548f204b28655ade70c63a0d76fee1ad583
jp772 <55673505+jp772@users.noreply.github.com>
2019-12-18 19:59:23 -0500

    Log Listener file listening to incoming errors
    This file is listening to all incoming errors from DMZ and DB servers and then creates/appends a .txt file.

---
+++ frontend/logging/log_listener.php
@@ -0,0 +1,61 @@
+<?php
+   
+    //Requried files
+    require_once('../rabbitmqphp_example/path.inc');
+    require_once('../rabbitmqphp_example/get_host_info.inc');
+    require_once('../rabbitmqphp_example/rabbitMQLib.inc');
+    
+    function storeError($error, $fileName){
+        $fp = fopen( $fileName . '.txt', "a" );
+        
+        for ($i = 0; $i < count($error); $i++){
+            fwrite( $fp, $error[$i] );
+        }
+        return true;
+    }
+    //This will route the request from server to function
+    function requestProcessor($request){
+       
+        if(!isset($request['type'])){
+            return array('message'=>"ERROR: Message type is not supported");
+        }
+        switch($request['type']){
+               
+            //  This case will handle all the requests from frontend   
+            case "frontend":
+                echo "<br>in Frontend listner: ";
+                $response_msg = storeError($request['error_string'], 'frontend_errors');
+                break;
+               
+            //  This case will handle all the requests from dmz
+            case "dmz":
+                
+                echo "<br>In dmz listner: ";
+                
+                $response_msg = storeError($request['error_string'], 'dmz_errors');
+                echo "Result: " . $response_msg;
+                break;
+         
+            //  This case will handle all the requests from db
+            case "db":
+                
+                echo "<br>In Database listner: ";
+                
+                $response_msg = storeError($request['error_string'], 'db_errors');
+                echo "Result: " . $response_msg;
+                break;
+       
+        }
+        echo $response_msg;
+        return $response_msg;
+    }
+    //creating a new server
+    $DMZLogServer = new rabbitMQServer('../rabbitmqphp_example/rabbitMQ_RMQ.ini', 'DMZ_RMQ_LOG_Server');
+    $DBLogServer = new rabbitMQServer('../rabbitmqphp_example/rabbitMQ_RMQ.ini', 'DB_RMQ_LOG_Server');
+   
+    //processes the request sent by client
+    $DMZLogServer->process_requests('requestProcessor');
+    $DBLogServer->process_requests('requestProcessor');
+   
+    //exit();
+    ?>
